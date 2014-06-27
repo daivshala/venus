f13a4c392509fd1cddb91320144be867fc124daa
Daniel Borlino de Oliveira <daniel.borlino@ingresse.com>
2014-06-27 14:42:37 -0300

    Página de documentação

---
+++ doc/app/scripts/controllers/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('venus-showcase')
+  .controller('MainCtrl', function ($scope, VenusActivityIndicatorService, $timeout) {
+    $scope.steps = [
+        {
+            text: 'Step 1',
+            url: '/'
+        },
+        {
+            text: 'Step 2',
+            url: '/2'
+        },
+        {
+            text: 'Step 3',
+            url: '/3'
+        },
+    ];
+
+    $scope.isButtonDisabled = true
+
+    $scope.activityTest = function(activityText){
+        VenusActivityIndicatorService.startActivity(activityText);
+        $timeout(function(){
+            VenusActivityIndicatorService.stopActivity(activityText);
+        }, 5000);
+    }
+
+    $scope.errorTest = function(errorText){
+        VenusActivityIndicatorService.error(errorText);
+    }
+  });
