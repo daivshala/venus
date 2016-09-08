707ad1fde37ccae4dbb3885e5edc1762c344382e
Udimberto Silvestre Junior <jSilverize@users.noreply.github.com>
2016-09-08 12:23:19 -0300

    add favicon (#7)
    remove older directives

--- tasks/prepareDist.js
+++ tasks/prepareDist.js
@@ -6,6 +6,7 @@ const gulp  = require('gulp'),
       SRC   = [
           paths.dist + '/index.html',
           paths.dist + '/assets/venus-docs.min.css',
+          paths.dist + '/assets/images/favicon.png',
           paths.dist + '/assets/images/application-logo*.svg',
           paths.dist + '/scripts/venus-docs.min.js',
       ];
